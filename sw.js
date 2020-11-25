00833b8ace0ca94462097a227034e9e6a40afc97
Alexis GOY <31611161+Alexis-GOY@users.noreply.github.com>
2020-11-25 16:18:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 var cacheName = 'appGinkoBus-v1';
 var appShellFiles = [
-  '/index.html',
-  '/app.js',
-  '/style.css',
-  '/icons/favicon.ico',
-  '/icons/icon-32.png',
-  '/icons/icon-64.png',
-  '/icons/icon-96.png',
-  '/icons/icon-128.png',
-  '/icons/icon-168.png',
-  '/icons/icon-180.png',
-  '/icons/icon-192.png',
-  '/icons/icon-256.png',
-  '/icons/icon-512.png',
-  '/icons/maskable_icon.png'
+  './index.html',
+  './app.js',
+  './style.css',
+  './icons/favicon.ico',
+  './icons/icon-32.png',
+  './icons/icon-64.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-168.png',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-256.png',
+  './icons/icon-512.png',
+  './icons/maskable_icon.png'
 ];
 
 self.addEventListener('install', (e) => {
